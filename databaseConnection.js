8a54cc8e93cf242e92a43b97e493ced51790a094
Gin Lu <glu23@my.bcit.ca>
2023-05-02 21:25:18 -0700

    Added my files for Assignment 2

---
+++ databaseConnection.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}?retryWrites=true&w=majority`;
+
+var database = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = { database };
